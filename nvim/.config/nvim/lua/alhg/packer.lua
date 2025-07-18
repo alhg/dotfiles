6476d85e31af856a8587db4ec8f10ca06a8b04b6
alhg <alhg@users.noreply.github.com>
2025-07-17 22:48:02 -0700

    Remove after/plugin/mason, run mason setup on packer config

--- nvim/.config/nvim/lua/alhg/packer.lua
+++ nvim/.config/nvim/lua/alhg/packer.lua
@@ -9,12 +9,14 @@ return require("packer").startup(function(use)
     })
 
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('theprimeagen/harpoon')
-    use('tpope/vim-fugitive')
+    use({'theprimeagen/harpoon'})
+    use({'tpope/vim-fugitive'})
 
     use({'neovim/nvim-lspconfig'})
-    use({'williamboman/mason.nvim'})
-    use({'williamboman/mason-lspconfig.nvim'})
+    use({
+        'williamboman/mason.nvim',
+        config = function() require('mason').setup() end
+    })
 
     use({'mangelozzi/nvim-rgflow.lua'})
 end)
