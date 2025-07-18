4e3523cb24f7c9691cfc0b477c330cfdbf590130
alhg <alhg@users.noreply.github.com>
2025-07-17 21:33:32 -0700

    Removed lsp, unused packages and config

--- nvim/.config/nvim/lua/alhg/packer.lua
+++ nvim/.config/nvim/lua/alhg/packer.lua
@@ -8,42 +8,13 @@ return require("packer").startup(function(use)
         requires = { {'nvim-lua/plenary.nvim'} }
     })
 
-    use({ 'rose-pine/neovim' })
-    use({
-        'folke/tokyonight.nvim',
-        config = function()
-            vim.cmd('colorscheme tokyonight-night')
-        end
-    })
-
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('nvim-treesitter/playground')
     use('theprimeagen/harpoon')
-    use('mbbill/undotree')
     use('tpope/vim-fugitive')
 
-    use({
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+    use({'neovim/nvim-lspconfig'})
+    use({'williamboman/mason.nvim'})
+    use({'williamboman/mason-lspconfig.nvim'})
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-
-            -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-        }
-    })
-
-    use('folke/zen-mode.nvim')
 end)
 
