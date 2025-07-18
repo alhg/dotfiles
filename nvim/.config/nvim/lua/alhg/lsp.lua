7e046b3dd8a6590fbf4da2373aed557ad631f0c1
alhg <alhg@users.noreply.github.com>
2025-07-17 22:47:27 -0700

    Update autocompletion usability

--- nvim/.config/nvim/lua/alhg/lsp.lua
+++ nvim/.config/nvim/lua/alhg/lsp.lua
@@ -5,8 +5,13 @@ vim.lsp.enable('rust-analyzer')
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then
+    if client and client:supports_method('textDocument/completion') then
       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
     end
   end,
 })
+
+-- Add the following options to completeopt to make it usable:
+--   - noselect :: Very annoying that autocomplete always selects, turn it off
+--   - fuzzy    :: I misspell all the time, also I use autocomplete to search methods quickly
+vim.cmd("set completeopt+=noselect,fuzzy")
