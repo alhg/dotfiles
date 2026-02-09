3ba179ff57002cc6590ab582ae0e99b521ede6de
alhg <alhg@users.norepy.github.com>
2026-02-08 16:56:45 -0800

    [neovim] Fallback to find_files for telescope when not in git repo

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,12 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', function()
+    local ok = pcall(builtin.git_files)
+    if not ok then
+        vim.notify("Not inside a git repo! Falling back to find_files()", vim.log.levels.INFO)
+        builtin.find_files()
+    end
+end, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
